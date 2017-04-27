39d59daca35f101e0d357987aac78a9ba9e1ca7f
Ella Xue <Ellaxue007@gmail.com>
2017-04-27 14:16:27 -0400

    build bplus tree and serialize to file

---
+++ p4/src/IO/TupleWriter.java
@@ -0,0 +1,24 @@
+package IO;
+
+import java.io.IOException;
+
+import project.Tuple;
+
+public interface TupleWriter {
+
+	
+/**
+ * write next tuple	
+ */
+public void writeNext(Tuple tu) throws IOException;
+
+public void writeNext(String str) throws IOException;
+
+
+/**
+ * close the writer
+ */
+public void close() throws IOException;
+
+public void writeHeader(String line) throws IOException;
+}
